692fc7515c9f7815eb782956f8966691c8ddb5e2
Great2008 <aakorokaragreatname@gmail.com>
2025-11-09 13:15:32 +0100

    update intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+// intro.js
+const name = "Azubuike Chris Okara"; // My Name
+const age = 25; // My age
+const favoriteLanguage = "JavaScript"; // My  favorite programming language
+
+console.log(`My name is ${name}.`);
+console.log(`I am ${age} years old.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
